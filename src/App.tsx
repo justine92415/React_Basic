6e8e13ff1389b8441487eb95e7c6f2995f781a82
justine92415 <justine92415@gmail.com>
2025-02-10 18:25:04 +0800

    feat(App):51.重構 Pizza 和 Order 組件以支援 props 解構

--- src/App.tsx
+++ src/App.tsx
@@ -65,8 +65,8 @@ function Header() {
 }
 
 function Menu() {
-  // const pizzas = pizzaData;
-  const pizzas: Array<PizzaData> = [];
+  const pizzas = pizzaData; 
+  // const pizzas: Array<PizzaData> = [];
   const numPizzas = pizzas.length;
 
   return (
@@ -115,7 +115,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -125,28 +125,28 @@ function Footer() {
   );
 }
 
-function Pizza(props: PizzaProps) {
-  console.log(props);
+function Pizza({ pizzaObj }: PizzaProps) {
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <div className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span> {props.pizzaObj.price} </span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span> {pizzaObj.price} </span>
       </div>
     </div>
   );
 }
 
-function Order(props: { closeHour: number }) {
+function Order({ openHour, closeHour }: HourProps) {
   return (
     <div className="order">
       <p>
-        We're open until until {props.closeHour}:00. Come visit us or order online.
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.
       </p>
       <button className="btn">Order</button>
     </div>
@@ -154,6 +154,11 @@ function Order(props: { closeHour: number }) {
 }
 
 export default App;
+
+interface HourProps {
+  openHour: number;
+  closeHour: number;
+}
 
 interface PizzaProps {
   pizzaObj: PizzaData;
