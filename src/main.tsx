cc006893fa334d4237f3646910ca9e4b34723458
justine92415 <justine92415@gmail.com>
2025-02-12 20:59:15 +0800

    feat(StarRating): 新增可重用的星級評分組件

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import './index.css'
+// import App from './App.tsx'
+import StarRating from './StarRating.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+  </StrictMode>
+);
