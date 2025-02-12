fd3fb77459b844fe5521a3df51fb6f9b760c1fb1
justine92415 <justine92415@gmail.com>
2025-02-12 21:27:49 +0800

    feat(StarRating):119.新增可重用的星級評分組件，支持自定義顏色、大小和評分信息

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -11,21 +11,32 @@ const starContainerStyle = {
   gap: '4px',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-};
-
-export default function StarRating({ maxRating = 5 }: StarRatingProps) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  message = [],
+  defaultRating = 0,
+  onSetRating,
+}: StarRatingProps) {
+  const [rating, setRating] = useState(defaultRating);
   const [tmpRating, setTmpRating] = useState(0);
 
   function handleRating(rating: number) {
     setRating(rating);
+    onSetRating && onSetRating(rating);
   }
 
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -34,18 +45,31 @@ export default function StarRating({ maxRating = 5 }: StarRatingProps) {
             full={tmpRating ? tmpRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTmpRating(i + 1)}
             onHoverOut={() => setTmpRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tmpRating || rating || ''}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tmpRating ? tmpRating - 1 : rating - 1]
+          : tmpRating || rating || ''}
+      </p>
     </div>
   );
 }
 
-function Star({ onRating, full, onHoverIn, onHoverOut }: StarProps) {
+function Star({
+  onRating,
+  full,
+  onHoverIn,
+  onHoverOut,
+  color,
+  size,
+}: StarProps) {
   const starStyle = {
-    width: '48px',
-    height: '48px',
+    width: `${size}px`,
+    height: `${size}px`,
     display: 'block',
     cursor: 'pointer',
   };
@@ -61,8 +85,8 @@ function Star({ onRating, full, onHoverIn, onHoverOut }: StarProps) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -71,7 +95,7 @@ function Star({ onRating, full, onHoverIn, onHoverOut }: StarProps) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
@@ -87,6 +111,12 @@ function Star({ onRating, full, onHoverIn, onHoverOut }: StarProps) {
 
 interface StarRatingProps {
   maxRating: number;
+  color?: string;
+  size?: number;
+  className?: string;
+  message?: string[];
+  defaultRating?: number;
+  onSetRating?: (rating: number) => void;
 }
 
 interface StarProps {
@@ -94,37 +124,6 @@ interface StarProps {
   onHoverIn: () => void;
   onHoverOut: () => void;
   full?: boolean;
+  color?: string;
+  size?: number;
 }
-
-/*
-FULL STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
->
-  <path
-    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-  />
-</svg>
-
-
-EMPTY STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
-
-*/
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css'
 // import App from './App.tsx'
@@ -7,6 +7,22 @@ import StarRating from './StarRating.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color="red" maxRating={3} defaultRating={2} />
+    <Test />
   </StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating maxRating={5} color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} starts</p>
+    </div>
+  );
+}
